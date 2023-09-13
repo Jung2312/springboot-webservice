ae52dcc98c42a0178f0369064824f92cf66c9f5c
JUNG\alswj <20212856@office.deu.ac.kr>
2023-09-13 12:53:35 +0900

    feat: connect_jpa

---
+++ src/main/java/org/springboot/service/posts/PostsService.java
@@ -0,0 +1,39 @@
+package org.springboot.service.posts;
+
+import org.springframework.transaction.annotation.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springboot.domain.posts.Posts;
+import org.springboot.domain.posts.PostsRepository;
+import org.springboot.web.dto.PostsResponseDto;
+import org.springboot.web.dto.PostsSaveRequestDto;
+import org.springboot.web.dto.PostsUpdateRequestDto;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    @Transactional(readOnly = true) // import org.springframework.transaction.annotation.Transactional; 로 import
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+}
