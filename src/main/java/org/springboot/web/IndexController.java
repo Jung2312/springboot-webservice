da52d2a53b42a22f5bc32e83188e94fb6bf6abad
JUNG\alswj <20212856@office.deu.ac.kr>
2023-10-04 21:02:17 +0900

    feat: add_swagger

--- src/main/java/org/springboot/web/IndexController.java
+++ src/main/java/org/springboot/web/IndexController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 // @RestController : @ResponseBody를 포함, 프론트에 전송할 필요가 없고 백에서만 데이터를 json으로 전송하고자 할 때 사용
 // @Controller : 컨트롤러는 자신을 호출했던 클라 쪽으로 View와 Model을 별도로 반환
